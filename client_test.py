6875ea881b00b0db61f4a32947876365c4921fe0
mikephu <mikephu03@gmail.com>
2024-03-24 00:43:15 -0700

    feat: client functionality and testing

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,18 +8,31 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes: 
+      self.assertAlmostEqual(getDataPoint(quote), (quote["stock"], quote["top_bid"]["price"], quote["top_ask"]["price"], (quote["top_bid"]["price"] + quote["top_ask"]["price"]) / 2))
+      
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes: 
+      self.assertAlmostEqual(getDataPoint(quote), (quote["stock"], quote["top_bid"]["price"], quote["top_ask"]["price"], (quote["top_bid"]["price"] + quote["top_ask"]["price"]) / 2))
+      
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculateRatio(self): 
+    prices = [(117.38, 115.595), (114.25, 115.595), (111.96, 112.035), (110.755, 112.035), (0, 1.282)]
+    
+    for price in prices:
+      self.assertAlmostEqual(getRatio(price[0], price[1]), (price[0] / price[1]))
 
-
+  def test_getRatio_divisionByZero(self): 
+    prices = [(117.38, 0), (5,0), (0, 0)]
+    
+    for price in prices:
+      self.assertIsNone(getRatio(price[0], price[1]))
 
 if __name__ == '__main__':
     unittest.main()
